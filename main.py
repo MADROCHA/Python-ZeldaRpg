3f691e9e6712b2594e2e88701a740444d2c799ba
MADROCHA <madlarocha@gmail.com>
2022-10-13 13:33:08 +0200

    python//camera_center:offstetY//_0.0

---
+++ main.py
@@ -0,0 +1,40 @@
+import pygame, sys, time
+from settings import *
+from level import Level
+#from debug import debug
+
+
+class Game:
+    def __init__(self):
+        
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        #self.display_surface = pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption('Zelda')
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+    def run(self):
+        last_time = time.time()
+        while True:
+            # delta time
+            dt = time.time() - last_time
+            last_time = time.time()
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            # update window
+            pygame.display.update()
+            # update window
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
